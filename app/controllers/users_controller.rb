2dd70a5bef1ebbc308e33eb4bfebacd555d649d5
Jason Mannon <jmannonpdx@gmail.com>
2014-04-10 09:58:10 -0700

    Added users and sessions controller, added index, and new view pages.  Working on sign up form and adding Log in capabilites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_controller)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Thanks for signing up"
+    else
+      render "new"
+    end
+  end
 end
