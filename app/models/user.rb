0ea201d2ba365a17f170248b8ecb8601005808e8
Jason Mannon <jmannonpdx@gmail.com>
2014-04-10 11:15:33 -0700

    Added validation and spec for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name, :presence => true
+  validates :password, :presence => true
+  validates :photo_id, :presence => true
+  has_many :photos
 end
--- db/migrate/20140410180923_add_photo_id.rb
+++ db/migrate/20140410180923_add_photo_id.rb
@@ -1,4 +1,5 @@
 class AddPhotoId < ActiveRecord::Migration
   def change
+    add_column :users, :photo_id, :int
   end
 end
